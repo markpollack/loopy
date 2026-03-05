ff1abea3657c2dc6ccfa3dfe4b980a15083620cb
Mark Pollack <mark.x.pollack@gmail.com>
2026-03-05 12:30:44 -0500

    Fix JBang script to use mavenlocal for SNAPSHOT dep

--- jbang/loopy.java
+++ jbang/loopy.java
@@ -1,7 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 21
-//REPOS central=https://repo1.maven.org/maven2/
-//REPOS central-snapshots=https://central.sonatype.com/repository/maven-snapshots/
+//REPOS mavenlocal
 //DEPS io.github.markpollack:loopy:0.1.0-SNAPSHOT
 
 import io.github.markpollack.loopy.LoopyApp;
